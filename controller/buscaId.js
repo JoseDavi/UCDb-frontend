57037b334ed0c542e1714228d344d6691d69da72
JoseDavi <jose.davi.souza@ccc.ufcg.edu.br>
2019-07-02 23:08:23 -0300

    pesquisa por id pegando

--- controller/buscaId.js
+++ controller/buscaId.js
@@ -1,8 +1,5 @@
-import "../webComponent/buscarId.js";
-import "../webComponent/psDisciplina.js";
-
 document.getElementById('btnBuscaId').onclick = () => {
-    const buscar = document.getElementById("txtBuscarId").value;
-    const disciplinas = document.getElementById('idDisciplina');
-    disciplinas.setAttribute("query", buscar);
+    const idBusca = document.getElementById("txtBuscarId").value;
+    localStorage.setItem("perfil", idBusca);
+    window.location.href = "../view/perfil.html";
 };
